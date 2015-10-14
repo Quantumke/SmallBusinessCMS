1b5f110718afd9ddea001b39de082865f4f5bd0d
Benson Mburu <nguruben@gmail.com>
2015-10-14 09:55:55 +0300

    Project Initialized

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>FLY FISHES</title>
+        <link rel="stylesheet" href="css/materialize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/materialize.js"></script>
+    </head>
+    <body>
+        
+        
+        
+    </body>
+</html>
